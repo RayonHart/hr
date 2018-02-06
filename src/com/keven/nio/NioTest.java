b2f66edccfa1088a7747a22d8cfe207a32df60f2
RayonHart <a360341540@163.com>
2018-02-06 15:26:50 +0800

    update NioTest.java and add hello.java

--- src/com/keven/nio/NioTest.java
+++ src/com/keven/nio/NioTest.java
@@ -2,7 +2,7 @@ package com.keven.nio;
 
 public class NioTest {
 	public static void main(String[] args) {
-		System.out.println("heheeh dadada");
-		System.out.println("aaa");
+		System.out.println("heheeh dadada222");
+		System.out.println("aaa111");
 	}
 }
